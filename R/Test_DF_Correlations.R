bf837dd1b0e2e21dad6d89d66d8dd2fcfc575f1f
David Hughes <hughes.evoanth@gmail.com>
2019-12-12 11:46:10 -0600

    updated function, error in outputing values when comparing factor on numeric

--- R/Test_DF_Correlations.R
+++ R/Test_DF_Correlations.R
@@ -24,12 +24,12 @@ Test_DF_Correlations = function(df){
         }else{
           if( class(x) == "factor"){
             test = factor_on_numeric(cat_values = x , num_values = y)
-            rhomat[i,j] = test[1]
-            pvalmat[i,j] = test[2]
+            rhomat[i,j] = test[2] ### rho
+            pvalmat[i,j] = test[4] ### pval
           } else {
             test = factor_on_numeric(cat_values = y , num_values = x)
-            rhomat[i,j] = test[2]
-            pvalmat[i,j] = test[4]
+            rhomat[i,j] = test[2] ### rho
+            pvalmat[i,j] = test[4] ### pval
             }
           }
         }
@@ -38,3 +38,41 @@ Test_DF_Correlations = function(df){
   out = list(RhoMat = rhomat, PvalMat = pvalmat)
   return(out)
   }
+
+#   Test_DF_Correlations = function(df){
+#   rhomat = pvalmat = matrix(NA, ncol(df), ncol(df))
+#   diag(rhomat) = diag(pvalmat) = 1
+#   for(i in 1:ncol(df)){
+#     for(j in 1:ncol(df)){
+#       x = unlist(df[,i]);  y = unlist(df[,j])
+#       if( class(x) == "factor" & class(y) == "factor" ){
+#         test = factor_on_factor(x,y)
+#         rhomat[i,j] = test[1]
+#         pvalmat[i,j] = test[3]
+#       } else{
+#         if(class(x) == "numeric" & class(y) == "numeric"){
+#           test = numeric_on_numeric(x,y)
+#           rhomat[i,j] = test[1]
+#           pvalmat[i,j] = test[2]
+#         }else{
+#           if( class(x) == "factor" & class(y) == "numeric"){
+#             test = factor_on_numeric(cat_values = x , num_values = y)
+#             rhomat[i,j] = test[1]
+#             pvalmat[i,j] = test[2]
+#           } else {
+#               if(class(x) == "numeric" & class(y) == "factor"){
+#                 test = factor_on_numeric(cat_values = y , num_values = x)
+#                 rhomat[i,j] = test[1]
+#                 pvalmat[i,j] = test[2]
+#               } else {
+#                 rhomat[i,j] = NA
+#                 pvalmat[i,j] = NA
+#               }
+#             }
+#           }
+#         }
+#       }
+#   }
+#   out = list(RhoMat = rhomat, PvalMat = pvalmat)
+#   return(out)
+#   }
